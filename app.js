a762e7bc376585b8419438086460b96e230a6818
igor <krzywakigor@gmail.com>
2021-01-16 15:08:27 +0000

    added delete feature

--- app.js
+++ app.js
@@ -22,6 +22,32 @@ UI.prototype.addBookToList = function (book) {
   list.appendChild(row);
 };
 
+// Show Alert
+UI.prototype.showAlert = function (message, className) {
+  // Create div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  //   Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#book-form');
+  //   Insert alert
+  container.insertBefore(div, form);
+  //   Timeout
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 3000);
+};
+
+// Delete book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 // Clear fields
 UI.prototype.clearFields = function () {
   document.getElementById('title').value = '';
@@ -30,6 +56,8 @@ UI.prototype.clearFields = function () {
 };
 
 // Event listeners
+
+// Add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
   // Get form values
   const title = document.getElementById('title').value,
@@ -42,11 +70,30 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
   //   Instantiate UI
   const ui = new UI();
 
-  //   Add book to list
-  ui.addBookToList(book);
+  //   Validate
+  if (title === '' || (author === '') | (isbn === '')) {
+    // Error Alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    //   Add book to list
+    ui.addBookToList(book);
 
-  //Clear fields
-  ui.clearFields();
+    // Show success
+    ui.showAlert('Book has been added!', 'success');
+
+    //Clear fields
+    ui.clearFields();
+  }
+
+  e.preventDefault();
+});
+// Delete Book
+document.getElementById('book-list').addEventListener('click', function (e) {
+  const ui = new UI();
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book removed', 'success');
 
   e.preventDefault();
 });
